799bf90a3dad977e879c2e70ecc7bffaeababccd
julitacreations <90911265+julitacreations@users.noreply.github.com>
2025-09-04 14:52:29 +0200

    initial tailwind template set up and bookshop added as well

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
+  
+  gem 'jekyll', '~>4.3.1'
+  gem 'webrick'
+  gem 'jekyll-sitemap'
+  gem 'csv'
+  gem 'base64'
+  gem 'logger'
 
-gem "jekyll", "4.3.3"
-gem "webrick"
-
+group :jekyll_plugins do
+  gem "jekyll-bookshop", "3.16.3"
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,59 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './_includes/**/*.{html,js,md}',
-    './_layouts/**/*.{html,js,md}',
-    './_*/*.{html,js,md}',
-    './*.{html,js,md}'
+    './_includes/**/*.{html,md}',
+    './_layouts/**/*.{html,md}',
+    './_*/**/*.{html,md}',
+    '!./_site/**/*.{html,md}',
+    './*.{html,md}',
+    './component-library/**/*.jekyll.html'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['Font Name', 'serif'],
+      sans: ['Font Name', 'sans-serif'],
+      body:  ['Font Name', 'sans-serif'],
+    },
+    safelist: [
+      {
+         pattern: /grid-cols-.*/,
+         variants: ['lg, md, sm'],
+      },
+      { pattern: /col-span-.*/,
+        variants: ['lg, md, sm'],
+       },
+     { pattern: /columns-.*/,
+     variants: ['lg, md, sm'],
+       },
+      ],
+    extend: {
+      aspectRatio: {
+        'portrait': '3/4',
+        'landscape': '16/9',  
+        'square': '1/1',
+      },
+      minHeight:{
+        '100': '100vh',
+        '90': '90vh',
+        '80': '80vh',
+        '70': '70vh',
+        '60': '60vh',
+        '50': '50vh',
+        '40': '40vh',
+        '30': '30vh',
+        '20': '20vh',
+      },
+      height:{
+        '100': '100vh',
+        '90': '90vh',
+        '80': '80vh',
+        '70': '70vh',
+        '60': '60vh',
+        '50': '50vh',
+        '40': '40vh',
+        '30': '30vh',
+        '20': '20vh',
+      },  
+    },
   },
   plugins: [],
 }
